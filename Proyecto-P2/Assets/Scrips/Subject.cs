7ad991fb751c583c3c847655b8dea72dd081b1f2
Chharli <144072056+Chharli@users.noreply.github.com>
2024-02-20 19:38:14 -0600

    main - proyecto escena 2

---
+++ Proyecto-P2/Assets/Scrips/Subject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Leccion
+{
+    public int ID;
+    public string lessons;
+    public List<string> opciones;
+    public int correctAnswer;
+}
+
+[CreateAssetMenu(fileName = "new Subject", menuName = "ScriptableObjects/NewLesson", order = 1)]
+public class Subject : ScriptableObject
+{
+    public List<Leccion> leccionList; 
+}
