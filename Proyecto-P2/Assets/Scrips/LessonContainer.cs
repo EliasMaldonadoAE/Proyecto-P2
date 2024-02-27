e512cd7617754b8dd283ebbffc8f0a208110d5e8
Chharli <144072056+Chharli@users.noreply.github.com>
2024-02-26 19:31:49 -0600

    main-Implement the lesson container scrip

---
+++ Proyecto-P2/Assets/Scrips/LessonContainer.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LessonContainer : MonoBehaviour
+{
+    [Header("GameObject Configuration")]
+    public int Lection = 0;
+    public int CurrentLession = 0;
+    public int TotalLessions = 0;
+    public bool AreAllLessonsComplete = false;
+
+    [Header("UI Configuration")]
+    public TMP_Text StageTitle;
+    public TMP_Text LessonStage;
+
+    [Header("External GameObject Configuration")]
+    public GameObject lessonContainer;
+
+    [Header("Lesson Data")]
+    public ScriptableObject LessonData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      if (lessonContainer != null)
+        {
+            OnUpdateUI();
+        }
+        else
+        {
+            Debug.LogWarning("GameObject nulo, revisa las variables de tipo GameObject lessonContainer");
+        }
+    }
+
+    public void OnUpdateUI()
+    {
+        if (StageTitle != null || LessonStage != null)
+        {
+            StageTitle.text = "Leccion " + Lection;
+            LessonStage.text = "Leccion " + CurrentLession + " de " + TotalLessions;
+        }
+        else
+        {
+            Debug.LogWarning("GameObject nulo, revisa las variables de tipo TMP_Text");
+        }
+    }
+
+
+
+    // Este metodo activa/desactiva la ventana de lessonContainer
+    public void EnableWindow()
+    {
+        OnUpdateUI();
+        if (lessonContainer.activeSelf)
+        {
+            //Desactiva el objecto si esta activo
+            lessonContainer.SetActive(false);
+        }
+        else
+        {
+            //Activa el objecto si esta desactivado
+            lessonContainer.SetActive(true);
+        }
+    }
+}
--- Proyecto-P2/Assets/Scrips/Subject.cs
+++ Proyecto-P2/Assets/Scrips/Subject.cs
@@ -7,12 +7,16 @@ public struct Leccion
 {
     public int ID;
     public string lessons;
-    public List<string> opciones;
+    public List<string> options;
     public int correctAnswer;
 }
 
 [CreateAssetMenu(fileName = "new Subject", menuName = "ScriptableObjects/NewLesson", order = 1)]
 public class Subject : ScriptableObject
 {
+    [Header("GameObject Configuration")]
+    public int Lesson = 0;
+
+    [Header ("Lession Quest Configuration")]
     public List<Leccion> leccionList; 
 }
